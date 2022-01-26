79dca8afd7b7e0a289c684f85ab6e574d8b335fa
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2022-01-26 13:46:29 +0100

    more powerful escape sequence engine

--- src/main/java/com/falsepattern/json/node/StringNode.java
+++ src/main/java/com/falsepattern/json/node/StringNode.java
@@ -51,22 +51,97 @@ public class StringNode extends JsonNode{
     }
 
     static @NotNull String deStringify(@NotNull @NonNull String text) {
-        return text.replace("\\\\", "\\")
-                   .replace("\\t", "\t")
-                   .replace("\\b", "\b")
-                   .replace("\\n", "\n")
-                   .replace("\\r", "\r")
-                   .replace("\\f", "\f")
-                   .replace("\\\"", "\"");
+        val sb = new StringBuilder();
+        val len = text.length();
+        for (int i = 0; i < len; i++) {
+            char c = text.charAt(i);
+            if (c == '\\') {
+                if (++i < len) {
+                    char next = text.charAt(i);
+                    switch (next) {
+                        case '"':
+                            sb.append('"');
+                            break;
+                        case '\\':
+                            sb.append('\\');
+                            break;
+                        case '/':
+                            sb.append('/');
+                            break;
+                        case 't':
+                            sb.append('\t');
+                            break;
+                        case 'b':
+                            sb.append('\b');
+                            break;
+                        case 'n':
+                            sb.append('\n');
+                            break;
+                        case 'r':
+                            sb.append('\r');
+                            break;
+                        case 'f':
+                            sb.append('\f');
+                            break;
+                        case 'u':
+                            if (i + 4 < len) {
+                                String hex = text.substring(i + 1, i + 5);
+                                try {
+                                    sb.append((char)Integer.parseInt(hex, 16));
+                                } catch (NumberFormatException e) {
+                                    throw new IllegalArgumentException("Invalid unicode escape sequence: \\u" + hex);
+                                }
+                                i += 4;
+                            } else {
+                                throw new IllegalArgumentException("Incomplete unicode escape sequence: \\u" + text.substring(i));
+                            }
+                    }
+                } else {
+                    throw new IllegalArgumentException("Incomplete escape sequence: \\");
+                }
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 
-    static @NotNull String stringify(@NotNull @NonNull String s) {
-        return s.replace("\\", "\\\\")
-                .replace("\t", "\\t")
-                .replace("\b", "\\b")
-                .replace("\n", "\\n")
-                .replace("\r", "\\r")
-                .replace("\f", "\\f")
-                .replace("\"", "\\\"");
+    static @NotNull String stringify(@NotNull @NonNull String text) {
+        val sb = new StringBuilder();
+        val len = text.length();
+        for (int i = 0; i < len; i++) {
+            char c = text.charAt(i);
+            switch (c) {
+                case '\\':
+                    sb.append("\\\\");
+                    break;
+                case '\t':
+                    sb.append("\\t");
+                    break;
+                case '\b':
+                    sb.append("\\b");
+                    break;
+                case '\n':
+                    sb.append("\\n");
+                    break;
+                case '\r':
+                    sb.append("\r");
+                    break;
+                case '\f':
+                    sb.append("\\f");
+                    break;
+                case '\"':
+                    sb.append("\\\"");
+                    break;
+                default:
+                    if (c < 32) {
+                        sb.append("\\u").append(String.format("%04x", (int)c));
+                    } else {
+                        sb.append(c);
+                    }
+            }
+
+        }
+        return sb.toString();
     }
 }
